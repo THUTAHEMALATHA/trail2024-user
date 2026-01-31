6111437dfbd14acc2c44e255fe914738383ce683
hemalatha <thutahemalatha@gmail.com>
2026-01-31 21:08:26 +0530

    creates controller

--- root/src/controllers/trip.controller.js
+++ root/src/controllers/trip.controller.js
@@ -1,4 +1,60 @@
 import express from "express";
-import {createTrip,getTrips,getTripById} from "../controllers/trip.controller.js";
+import { supabase } from "../config/supabase";
 
-const router = express.Router();
+export const createTrip = async(req,res)=>{
+    const {customer_id, vehicle_id,distance km}=req.body;
+    const {data:vehicle}=await supabase
+    .from('vehicles5'
+    .select('*')
+    .eq('id',vehicle_id)
+    .single()
+
+    if(!vechicle.isavailable){
+        return
+        res.status(400).json({error:'vehicle is not available'});
+
+    }
+    const {data,error}=await
+    supabase
+    .from(trips5)
+    .insert([{
+        customer_id
+  vehicle_id ,
+distance_km   }])
+    )
+    await supabase
+    .from(vehicles5)
+    .update({isavailable:false})
+    .eq('id',vehicle_id)
+
+    if(error){
+        return
+        res.status(400).json({error: error.message})
+    }
+    res.status(201).json(data)
+}
+
+export const getTrip = async (req,res)=>{
+    const {tripId}=req.params;
+    const {data,error}=await supabase
+    .from('trips5')
+    .select('*')
+    .eq('id',tripId)
+    .single()
+    if(error){
+        return res.status(400).json({error:error.message})
+    }
+    res.status(200).json(data)
+}
+export const updateTrip = async (req,res)=>{
+    const {tripId}=req.params;
+    const {distance_km}=req.body;
+    const {data,error}=await supabase
+    .from('trips5')
+    .update({distance_km})
+    .eq('id',tripId)    
+    
+    if(error){
+        return res.status(400).json({error:error.message})
+    }   
+    
